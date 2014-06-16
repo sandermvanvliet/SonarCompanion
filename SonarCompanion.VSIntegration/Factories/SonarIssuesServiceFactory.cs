b8b192c77f88df8ca529568a9dbffbefb07bcac9
Sander van Vliet <msn@barad-dur.nl>
2014-06-16 10:02:45 +0200

    Added the actual issues service back in

--- SonarCompanion.VSIntegration/Factories/SonarIssuesServiceFactory.cs
+++ SonarCompanion.VSIntegration/Factories/SonarIssuesServiceFactory.cs
@@ -6,9 +6,17 @@ namespace Rabobank.SonarCompanion_VSIntegration.Factories
     [Export]
     public class SonarIssuesServiceFactory
     {
+        private readonly ISonarOptionsService _sonarOptionsService;
+
+        [ImportingConstructor]
+        public SonarIssuesServiceFactory(ISonarOptionsService sonarOptionsService)
+        {
+            _sonarOptionsService = sonarOptionsService;
+        }
+
         public ISonarIssuesService Create()
         {
-            return new DummySonarIssuesService();
+            return new SonarIssuesService(_sonarOptionsService);
         }
     }
 }
---
+++ SonarCompanion.VSIntegration/Services/SonarIssuesService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SonarCompanion.API;
+
+namespace Rabobank.SonarCompanion_VSIntegration.Services
+{
+    public class SonarIssuesService : ISonarIssuesService
+    {
+        private readonly SonarService _sonarService;
+        private List<SonarIssue> _sonarIssues;
+
+        public SonarIssuesService(ISonarOptionsService sonarOptionsService)
+        {
+            var sonarUri = new Uri(sonarOptionsService.GetOptions().SonarUrl);
+
+            _sonarService = new SonarService(sonarUri);
+        }
+
+        public SonarIssue GetIssueFor(string fileName, int lineNumber)
+        {
+            return GetIssuesForFile(fileName)
+                .FirstOrDefault(issue => issue.Line == lineNumber);
+        }
+
+        public IEnumerable<SonarIssue> GetIssuesForFile(string fileName)
+        {
+            return
+                _sonarIssues.Where(
+                    issue => string.Equals(issue.FileName, fileName, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public List<SonarProject> GetProjects()
+        {
+            return _sonarService.GetProjects();
+        }
+
+        public IEnumerable<SonarIssue> GetAllIssues(string key, Action<int> updateProgress)
+        {
+            _sonarIssues = _sonarService.GetAllIssues(key, updateProgress);
+
+            return _sonarIssues;
+        }
+    }
+}
