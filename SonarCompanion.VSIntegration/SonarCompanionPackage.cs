f954e92b185fd120797d0b1ffb54f7d580b9850b
Sander van Vliet <msn@barad-dur.nl>
2014-09-04 06:47:38 +0200

    Cleaned up the package some

--- SonarCompanion.VSIntegration/SonarCompanionPackage.cs
+++ SonarCompanion.VSIntegration/SonarCompanionPackage.cs
@@ -1,64 +1,34 @@
 ﻿using System;
 using System.ComponentModel.Design;
 using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using log4net;
+using log4net.Config;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
-using SonarCompanion_VSIntegration.Controls;
 using SonarCompanion_VSIntegration.Interop;
 using SonarCompanion_VSIntegration.MessageBus;
 using SonarCompanion_VSIntegration.Services;
 
 namespace SonarCompanion_VSIntegration
 {
-    /// <summary>
-    ///     This is the class that implements the package exposed by this assembly.
-    ///     The minimum requirement for a class to be considered a valid package for Visual Studio
-    ///     is to implement the IVsPackage interface and register itself with the shell.
-    ///     This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    ///     to do it: it derives from the Package class that provides the implementation of the
-    ///     IVsPackage interface and uses the registration attributes defined in the framework to
-    ///     register itself and its components with the shell.
-    /// </summary>
-    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-    // a package.
     [PackageRegistration(UseManagedResourcesOnly = true)]
-
-    // This attribute is used to register the information needed to show this package
-    // in the Help/About dialog of Visual Studio.
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-
-    // This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource("Menus.ctmenu", 1)]
-
-    // This attribute registers a tool window exposed by this package.
     [ProvideToolWindow(typeof (SonarIssuesToolWindow))]
     [Guid(GuidList.guidSonarCompanion_VSIntegrationPkgString)]
     [ProvideOptionPage(typeof (SonarOptionsPage), "Sonar Companion", "General", 0, 0, true)]
     public sealed class SonarCompanionPackage : Package
     {
-        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private SolutionEventsSink _solutionEventsSink;
 
-        /// <summary>
-        ///     This function is called when the user clicks the menu item that shows the
-        ///     tool window. See the Initialize method to see how the menu item is associated to
-        ///     this function using the OleMenuCommandService service and the MenuCommand class.
-        /// </summary>
-        /// <param name="sender">
-        ///     The sender.
-        /// </param>
-        /// <param name="e">
-        ///     The e.
-        /// </param>
         private void ShowToolWindow(object sender, EventArgs e)
         {
-            // Get the instance number 0 of this tool window. This window is single instance so this instance
-            // is actually the only one.
-            // The last flag is set to true so that if the tool window does not exists it will be created.
-            ToolWindowPane window = FindToolWindow(typeof (SonarIssuesToolWindow), 0, true);
+            var window = FindToolWindow(typeof (SonarIssuesToolWindow), 0, true);
             if ((null == window) || (null == window.Frame))
             {
                 throw new NotSupportedException(Resources.CanNotCreateWindow);
@@ -68,13 +38,9 @@ namespace SonarCompanion_VSIntegration
             ErrorHandler.ThrowOnFailure(windowFrame.Show());
         }
 
-        /// <summary>
-        ///     Initialization of the package; this method is called right after the package is sited, so this is the place
-        ///     where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
         protected override void Initialize()
         {
-            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config"));
+            XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config"));
             Log.Info("Initializing Sonar Companion.");
 
             AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
@@ -88,14 +54,13 @@ namespace SonarCompanion_VSIntegration
             }
 
             var messageBus = componentModel.GetService<IMessageBus>();
-            
+
             // Compose services that depend on messagebus
             componentModel.GetService<ISonarIssuesService>();
             componentModel.GetService<IVisualStudioAutomationService>();
-                
-            _solutionEventsSink = new SolutionEventsSink(messageBus, GetService(typeof(SVsSolution)) as IVsSolution);
 
-            
+            _solutionEventsSink = new SolutionEventsSink(messageBus, GetService(typeof (SVsSolution)) as IVsSolution);
+
             // Add our command handlers for menu (commands must exist in the .vsct file)
             var mcs = GetService(typeof (IMenuCommandService)) as OleMenuCommandService;
 
@@ -126,7 +91,7 @@ namespace SonarCompanion_VSIntegration
             }
         }
 
-        private void HandleUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        private static void HandleUnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             Log.Error(e.ExceptionObject);
         }
