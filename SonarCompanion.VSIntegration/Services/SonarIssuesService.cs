661d34ca64590f62f02b5ebf9b20d199b856859d
Sander van Vliet <msn@barad-dur.nl>
2014-09-04 14:04:19 +0200

    Added sample configuration file and updated README

--- SonarCompanion.VSIntegration/Services/SonarIssuesService.cs
+++ SonarCompanion.VSIntegration/Services/SonarIssuesService.cs
@@ -89,12 +89,4 @@ namespace SonarCompanion_VSIntegration.Services
             }
         }
     }
-
-    public class SonarCompanionSettingKeys
-    {
-        public const string SonarUri = "sonarUri";
-        public const string DefaultProject = "defaultProjectName";
-        public const string AutoRefreshEnabled = "autoRefresh.enabled";
-        public const string AutoRefreshInterval = "autoRefresh.interval";
-    }
 }
---
+++ SonarCompanion.VSIntegration/SonarCompanionSettingKeys.cs
@@ -0,0 +1,10 @@
+namespace SonarCompanion_VSIntegration
+{
+    public class SonarCompanionSettingKeys
+    {
+        public const string SonarUri = "sonarUri";
+        public const string DefaultProject = "defaultProjectName";
+        public const string AutoRefreshEnabled = "autoRefresh.enabled";
+        public const string AutoRefreshInterval = "autoRefresh.interval";
+    }
+}
